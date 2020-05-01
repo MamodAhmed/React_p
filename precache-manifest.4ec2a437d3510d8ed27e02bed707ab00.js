e620199a9e718dd49433d29bdaf8776b6c57a1fb
Mahmoud Ahmed <MahmoAhmed@gmail.com>
2020-05-01 11:15:31 +0200

    Updates

---
+++ precache-manifest.4ec2a437d3510d8ed27e02bed707ab00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfcfc46dbec6b723d94fe1f8e4e304d",
+    "url": "/React_p/index.html"
+  },
+  {
+    "revision": "a3acbb5e920309b97426",
+    "url": "/React_p/static/css/main.6ac61e15.chunk.css"
+  },
+  {
+    "revision": "c34eff9ee469c42e927c",
+    "url": "/React_p/static/js/2.1248dd3a.chunk.js"
+  },
+  {
+    "revision": "3d72a9594b3c4ca43da4afa8e1d7849a",
+    "url": "/React_p/static/js/2.1248dd3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3acbb5e920309b97426",
+    "url": "/React_p/static/js/main.8271a1ae.chunk.js"
+  },
+  {
+    "revision": "659fc3366cd12b0bbe18",
+    "url": "/React_p/static/js/runtime-main.26958988.js"
+  }
+]);
